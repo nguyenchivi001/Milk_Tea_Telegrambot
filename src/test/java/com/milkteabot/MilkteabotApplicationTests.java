1222e5d1dd50bdff3c390bb72794864d21d17b8a
nguyenchivi001 <nguyenchivi292004@gmail.com>
2026-03-11 01:52:42 +0700

    Test: Add unit test for Menu and Order service

--- src/test/java/com/milkteabot/MilkteabotApplicationTests.java
+++ src/test/java/com/milkteabot/MilkteabotApplicationTests.java
@@ -2,14 +2,9 @@ package com.milkteabot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootTest
 class MilkteabotApplicationTests {
-
-	@MockBean
-	TelegramBotsApi telegramBotsApi;
 
 	@Test
 	void contextLoads() {
